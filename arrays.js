cfe21aba56771fb6156b6306c3cb0ba0a882a248
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-05 21:59:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,6 +28,6 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  console.log(accessElementInArray[0]);
+  console.log(array[0]);
   
 }
