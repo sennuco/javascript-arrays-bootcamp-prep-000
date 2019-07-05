f48137764aa296b01057e059bd02928858fb7ca8
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-05 22:17:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -50,6 +50,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-array.slice(0, array.length - 1);
+return array.slice(0, array.length - 1);
 
 }
