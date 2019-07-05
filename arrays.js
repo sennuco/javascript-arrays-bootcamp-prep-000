bceb44bfc3898376ae2f5e66f9353e9b0200e172
Theresa Cyrus <cyrustheresa@gmail.com>
2019-07-05 22:08:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,7 +39,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-array = array.slice();
+array = array.slice(1);
 return array;
   
 }
